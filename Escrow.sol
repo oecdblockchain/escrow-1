0e69542aea5114901a0f1580812ecf1ce75c273e
oecdblockchain <44101692+oecdblockchain@users.noreply.github.com>
2020-09-18 23:17:18 +0200

    Update Escrow.sol

--- Escrow.sol
+++ Escrow.sol
@@ -1,152 +1,120 @@
-pragma solidity ^0.4.21;
-
-import "./Ownable.sol";
-
-/**
- * Escrow smart contract serves 3 parties.
- * 
- * Judge = is able to move funds to flexiana or customer.
- * Customer = Flexiana's client
- * Flexiana = we 
- * 
- * After creating contract, setAddressesOnce() have to be called.
- * 
- * @author Jiri Knesl <jiri@flexiana.com>
- */
-contract Escrow is Ownable
-{
+pragma solidity 0.5.12;
+contract Escrow{
+    address public arbiter;
+    address payable public customer;
+    address payable public supplier;
+    uint8 public UNCLAIMED = 10;
+    uint8 public PAY_CLAIMED = 20;
+    uint8 public REFUND_CLAIMED = 25;
+    uint8 public PAY_CLAIM_APPROVED = 30;
+    uint8 public REFUND_CLAIM_APPROVED = 35;
+    uint8 public ESCALATED = 40;
+    uint8 public SETTLED = 50;
+    uint8 public PAID = 60;
+    uint8 public state = UNCLAIMED;
+    uint256 public settlement;
     
-    /*** SECTION: Internal state & modifiers */
-    
-    enum Statuses {DoNothing, ShouldTransferToCustomer, ShouldTransferToFlexiana}
 
-    bool public addressChanged = false;
-    address public customer;
-    address public flexiana;
-    // address public owner; // owner = independent judge
-    Escrow.Statuses public customerStatus = Statuses.DoNothing;
-    Escrow.Statuses public flexianaStatus = Statuses.DoNothing;
+    event PaymentDone(uint256 amount, address recipient);
+    event StateChange(uint8 oldState, uint8 newState);
 
-    modifier onlyCustomer() {
-        require(msg.sender == customer);
-        _;
+    constructor(address payable _customer, 
+                address payable _supplier,
+                address payable _arbiter) public{
+
+        customer = _customer;
+        supplier = _supplier;
+        arbiter  = _arbiter;
     }
 
-    modifier onlyFlexiana() {
-        require(msg.sender == flexiana);
-        _;
-    }
-    
-    /*** SECTION: Initialization */
-    
-    /**
-     * After intitialization setAddressesOnce is called with all members addresses.
-     * This method can be called only once and only by contract creator.
-     */
-    function setAddressesOnce(address judge, address newFlexiana, address newCustomer) public onlyOwner {
-        require(! addressChanged);
-        addressChanged = true;
-        owner = judge;
-        flexiana = newFlexiana;
-        customer = newCustomer;
-    }
+    function () external payable{}
 
-    /*** SECTION: Changing customer status */
-    
-    function changeCustomerStatus(Escrow.Statuses newStatus) internal onlyCustomer {
-        customerStatus = newStatus;
-    }
-    
-    /**
-     * Called when customer wants funds to stay in the Escrow
-     */
-    function changeCustomerStatusToDoNothing() public onlyCustomer {
-        changeCustomerStatus(Statuses.DoNothing);
-    }
-    
-    /**
-     * Called when customer wants to return funds back.
-     */
-    function changeCustomerStatusToShouldTransferToCustomer() public onlyCustomer {
-        changeCustomerStatus(Statuses.ShouldTransferToCustomer);
-    }
-    
-    /**
-     * Called when customer wants to transfer funds to flexiana.
-     */
-    function changeCustomerStatusToShouldTransferToFlexiana() public onlyCustomer {
-        changeCustomerStatus(Statuses.ShouldTransferToFlexiana);
-    }
-    
-    /*** SECTION: Changing Flexiana status */
-    
-    function changeFlexianaStatus(Escrow.Statuses newStatus) internal onlyFlexiana {
-        flexianaStatus = newStatus;
-    }
-
-    /**
-     * Flexiana wants to hold funds in the escrow.
-     */
-    function changeFlexianaStatusToDoNothing() public onlyFlexiana {
-        changeFlexianaStatus(Statuses.DoNothing);
-    }
-    
-    /**
-     * Flexiana wants to send funds back to customer.
-     */
-    function changeFlexianaStatusToShouldTransferToCustomer() public onlyFlexiana {
-        changeFlexianaStatus(Statuses.ShouldTransferToCustomer);
-    }
-    
-    /**
-     * Client wants to return money.
-     */
-    function changeFlexianaStatusToShouldTransferToFlexiana() public onlyFlexiana {
-        changeFlexianaStatus(Statuses.ShouldTransferToFlexiana);
-    }
-    
-    /*** SECTION: Deposits */
-    
-    /**
-     * Let's save amount in smart contract.
-     */
-    function deposit() public payable {
-    }
-
-    /**
-     * Safer way how to store funds. It requires only customer to deposit funds.
-     */
-    function customerDeposit() public payable onlyCustomer {
-    }
-    
-    /*** SECTION: Transfers */
-
-    /**
-     * If Flexiana agrees or if judge decides, funds are transfered do customer.
-     */
-    function transferToCustomer() public {
-        require(flexianaStatus == Statuses.ShouldTransferToCustomer || msg.sender == owner);
-        customer.transfer(address(this).balance);
-    }
-
-    /**
-     * If client agrees or if judge decides, funds are transfered to flexiana.
-     */
-    function transferToFlexiana() public {
-        require(customerStatus == Statuses.ShouldTransferToCustomer || msg.sender == owner);
-        flexiana.transfer(address(this).balance);
-    }
-
-    /**
-     * Withdraw funds to party that should have funds.
-     */
-    function withdraw() public {
-        require(flexianaStatus != Statuses.DoNothing && flexianaStatus == customerStatus);
-        require(msg.sender == owner || msg.sender == flexiana || msg.sender == customer);
-        if (flexianaStatus == Statuses.ShouldTransferToCustomer) {
-            customer.transfer(address(this).balance);
-        } else if (flexianaStatus == Statuses.ShouldTransferToFlexiana) {
-            flexiana.transfer(address(this).balance);
+    function claim() external{
+        require(state == UNCLAIMED);
+        require(msg.sender == customer || msg.sender == supplier);
+        
+        if(msg.sender == customer){
+            state = REFUND_CLAIMED;
+            emit StateChange(UNCLAIMED, REFUND_CLAIMED);
+        } 
+        else{
+            state = PAY_CLAIMED;
+            emit StateChange(UNCLAIMED, PAY_CLAIMED);            
         }
     }
+    
+    function reject() external{
+        require(state == PAY_CLAIMED || state == REFUND_CLAIMED);
+        if(state == PAY_CLAIMED) require(msg.sender == customer);
+        if(state == REFUND_CLAIMED) require(msg.sender == supplier);
+
+        uint8 oldState = state;
+        state = ESCALATED;
+        emit StateChange (oldState, state);
+    }    
+    
+    function approve() external{
+        require(state == PAY_CLAIMED || state == REFUND_CLAIMED);
+
+        uint8 oldState = state;
+        if(state == PAY_CLAIMED){
+            require(msg.sender == customer);
+            state = PAY_CLAIM_APPROVED;
+        }
+
+        if(state == REFUND_CLAIMED){
+            require(msg.sender == supplier);
+            state = REFUND_CLAIM_APPROVED;
+        }
+ 
+        emit StateChange(oldState, state);
+    }
+
+    function settle(uint256 _settlement) external{
+        require(state == ESCALATED);
+        require(msg.sender == arbiter);
+        uint256 balance = address(this).balance;
+        require (_settlement <= balance);
+
+        settlement = _settlement;
+        state = SETTLED;
+        emit StateChange(ESCALATED, SETTLED);
+    }
+
+    
+    function withdraw() external{
+        require (state == PAY_CLAIM_APPROVED || state == REFUND_CLAIM_APPROVED || state == SETTLED);
+        uint8 oldState = state;
+        state = PAID;
+        emit StateChange(oldState, state);
+
+        uint256 balance = address(this).balance;
+
+        if(oldState == PAY_CLAIM_APPROVED){
+            supplier.transfer(balance);
+            emit PaymentDone(balance , supplier);
+        }
+
+        if(oldState == REFUND_CLAIM_APPROVED){
+            customer.transfer(balance);
+            emit PaymentDone(balance , customer);
+        }
+
+
+        if(oldState == SETTLED){
+            require(settlement <= balance);
+            supplier.transfer(settlement);
+            emit PaymentDone(settlement , supplier);
+            customer.transfer(balance - settlement);
+            emit PaymentDone(balance - settlement , customer);
+            
+        }
+
+    }
+    
+
+    
+    
+    
+    
 }
